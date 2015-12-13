6c2f63bb7d5f87d1760cb21ded8db27b8f292423
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-13 12:48:23 +0800

    Reformated test cases for Intermediate bonfire 4 to be read cleaner.

--- test/bonfires/intermediate-algorithm-scripting/test-cases/04-test.js
+++ test/bonfires/intermediate-algorithm-scripting/test-cases/04-test.js
@@ -2,59 +2,124 @@ module.exports = [
 	{ // case one
 		"input": [
 			[
-				{ first: "Romeo", last: "Montague" },
-				{ first: "Mercutio", last: null },
-				{ first: "Tybalt", last: "Capulet" }
+				{
+					first: "Romeo",
+					last: "Montague"
+				},
+				{
+					first: "Mercutio",
+					last: null
+				},
+				{
+					first: "Tybalt",
+					last: "Capulet"
+				}
 			],
-			{ last: "Capulet" }
+			{
+				last: "Capulet"
+			}
 		],
 		"expected": [
-			{ first: "Tybalt", last: "Capulet" }
+			{
+				first: "Tybalt",
+				last: "Capulet"
+			}
 		]
 	}, // end case one
 
 	{ // case two
 		"input": [
 			[
-				{ "a": 1 },
-				{ "a": 1 },
-				{ "a": 1, "b": 2 }
+				{
+					"a": 1
+				},
+				{
+					"a": 1
+				},
+				{
+					"a": 1,
+					"b": 2
+				}
 			],
-			{ "a": 1 }
+			{
+				"a": 1
+			}
 		],
 		"expected": [
-			{ "a": 1 },
-			{ "a": 1 },
-			{	"a": 1, "b": 2 }
+			{
+				"a": 1
+			},
+			{
+				"a": 1
+			},
+			{
+				"a": 1,
+				"b": 2
+			}
 		]
 	}, // end case two
 
 	{ // case three
 		"input": [
 			[
-				{ "a": 1, "b": 2 },
-				{ "a": 1 },
-				{ "a": 1, "b": 2, "c": 2}
+				{
+					"a": 1,
+					"b": 2
+				},
+				{
+					"a": 1
+				},
+				{
+					"a": 1,
+					"b": 2,
+					"c": 2
+				}
 			],
-			{ "a": 1,"b": 2}
+			{
+				"a": 1,
+				"b": 2
+			}
 		],
 		"expected": [
-			{	"a": 1,	"b": 2 },
-			{	"a": 1, "b": 2, "c": 2 }
+			{
+				"a": 1,
+				"b": 2
+			},
+			{
+				"a": 1,
+				"b": 2,
+				"c": 2
+			}
 		]
 	}, // end case three
 
-	{
+	{ // case four
 		"input": [
 			[
-				{ "a": 1, "b": 2 },
-				{ "a": 1 },
-				{ "a": 1, "b": 2, "c": 2 }
+				{
+					"a": 1,
+					"b": 2
+				},
+				{
+					"a": 1
+				},
+				{
+					"a": 1,
+					"b": 2,
+					"c": 2
+				}
 			],
-			{ "a": 1, "c": 2 }
+			{
+				"a": 1,
+				"c": 2
+			}
 		],
 		"expected": [
-			{ "a": 1, "b": 2, "c": 2 }
+			{
+				"a": 1,
+				"b": 2,
+				"c": 2
+			}
 		]
-	}
+	} // end case four
 ];
