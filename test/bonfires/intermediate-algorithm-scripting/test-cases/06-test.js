02d1fb8a1d7e9636df5a0e0a4a93fa7669310610
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-14 06:27:53 +0800

    Added test cases for Intermediate Bonfire 06: Pig Latin.

---
+++ test/bonfires/intermediate-algorithm-scripting/test-cases/06-test.js
@@ -0,0 +1,22 @@
+module.exports = [
+	{
+		"input": "california",
+		"expected": "aliforniacay"
+	},
+	{
+		"input": "paragraphs",
+		"expected": "aragraphspay"
+	},
+	{
+		"input": "glove",
+		"expected": "oveglay"
+	},
+	{
+		"input": "algorithm",
+		"expected": "algorithmway"
+	},
+	{
+		"input": "eight",
+		"expected": "eightway"
+	}
+];
