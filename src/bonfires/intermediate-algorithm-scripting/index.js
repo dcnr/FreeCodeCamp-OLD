9a9b88ebcd95b7ed2d8e0fcab5472cb11d309e7b
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-15 06:16:40 +0800

    Added base files for Bonfire: Missing Letters.

--- src/bonfires/intermediate-algorithm-scripting/index.js
+++ src/bonfires/intermediate-algorithm-scripting/index.js
@@ -5,7 +5,8 @@ var IntermediateAlgo = {
 	"where": require("./04-where-art-thou"),
 	"myReplace": require("./05-search-and-replace"),
 	"translate": require("./06-pig-latin"),
-	"pair": require("./07-dna-pairing")
+	"pair": require("./07-dna-pairing"),
+	"fearNotLetter": require("./08-missing-letters")
 };
 
 module.exports = IntermediateAlgo;
