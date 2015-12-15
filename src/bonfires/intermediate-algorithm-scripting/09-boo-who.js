ec107dfb9a7af665fa395bdbf0359764b7b23f42
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-15 09:25:43 +0800

    Added test spec.

---
+++ src/bonfires/intermediate-algorithm-scripting/09-boo-who.js
@@ -0,0 +1,20 @@
+/* Bonfire: Boo who
+ * http://www.freecodecamp.com/challenges/bonfire-boo-who
+ *
+ * Check if a value is classified as a boolean primitive.
+ * Return true or false.
+ *
+ * Boolean primitives are true and false.
+ *
+ */
+
+
+function boo(bool) {
+	var result = true;
+
+	return result;
+}
+
+
+/* exports */
+module.exports = boo;
