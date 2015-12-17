d37183e1795c58b2f216ac78ad3e4e2ba0fe6898
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-18 05:40:50 +0800

    Added base files for Bonfire: Drop It.

---
+++ src/bonfires/intermediate-algorithm-scripting/17-drop-it.js
@@ -0,0 +1,17 @@
+/* Bonfire: Drop it
+ * http://www.freecodecamp.com/challenges/bonfire-drop-it
+ *
+ *Drop the elements of an array (first argument), starting
+ *from the front, until the predicate (second argument) returns true.
+ *
+ */
+
+
+function drop(arr, func) {
+	// Drop them elements.
+	return arr;
+}
+
+
+/* exports */
+module.exports = drop;
