e89bd20203e4c4f2533c414a9701927a3cec0cd8
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-17 10:18:13 +0800

    Added base files for Bonfire: Smalles Common Multiple

---
+++ src/bonfires/intermediate-algorithm-scripting/15-smalles-common-multiple.js
@@ -0,0 +1,23 @@
+/* Bonfire: Smallest Common Multiple
+ * http://www.freecodecamp.com/challenges/bonfire-smallest-common-multiple
+ *
+ * Find the smallest common multiple of the provided parameters that
+ * can be evenly divided by both, as well as by all sequential numbers
+ * in the range between these parameters.
+ *
+ * The range will be an array of two numbers that will not necessarily
+ * be in numerical order.
+ *
+ * e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3
+ * that is evenly divisible by all numbers between 1 and 3.
+ *
+ */
+
+
+function smallestCommons(arr) {
+	return arr;
+}
+
+
+/* exports */
+module.exports = smallestCommons;
