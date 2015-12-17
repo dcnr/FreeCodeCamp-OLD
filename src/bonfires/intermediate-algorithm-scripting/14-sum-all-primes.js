b018d554339cf80c8be742d7f1ca7fe7dc62bed2
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-17 08:15:21 +0800

    Added spec and test cases.

---
+++ src/bonfires/intermediate-algorithm-scripting/14-sum-all-primes.js
@@ -0,0 +1,21 @@
+/* Bonfire: Sum All Primes
+ * http://www.freecodecamp.com/challenges/bonfire-sum-all-primes
+ *
+ * Sum all the prime numbers up to and including the provided number.
+ *
+ * A prime number is defined as having only two divisors, 1 and itself.
+ * For example, 2 is a prime number because it's only divisible by 1 and 2.
+ * 1 isn't a prime number, because it's only divisible by itself.
+ *
+ * The provided number may not be a prime.
+ *
+ */
+
+
+function sumPrimes(num) {
+	return num;
+}
+
+
+/* exports */
+module.exports	= sumPrimes;
---
+++ test/bonfires/intermediate-algorithm-scripting/14-sum-all-primes-spec.js
@@ -0,0 +1,33 @@
+module.exports = function () {
+	'use strict';
+
+	var expect = require("chai")
+		.expect;
+
+	var sumPrimes = require("./intermediate-algo-collection")
+		.sumPrimes;
+
+	var tests = require("./test-cases/14-test.js");
+
+	describe("Sum All Primes", () => {
+		describe("sumPrimes()", () => {
+			it("should return a number", () => {
+				tests.forEach(test => {
+					let result = sumPrimes(test.input);
+
+					expect(result)
+						.to.be.a("number");
+				});
+			});
+
+			it("should return the sum of all primes up to given number", () => {
+				tests.forEach(test => {
+					let result = sumPrimes(test.input);
+
+					expect(result)
+						.to.equal(test.expected);
+				});
+			});
+		});
+	});
+};
