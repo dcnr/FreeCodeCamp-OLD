6861f0d4468afc2aff50a46ca048faac5309cee2
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-18 06:02:37 +0800

    Completed Bonfire: Drop It. Resolves #30

--- src/bonfires/intermediate-algorithm-scripting/17-drop-it.js
+++ src/bonfires/intermediate-algorithm-scripting/17-drop-it.js
@@ -1,15 +1,22 @@
 /* Bonfire: Drop it
  * http://www.freecodecamp.com/challenges/bonfire-drop-it
  *
- *Drop the elements of an array (first argument), starting
- *from the front, until the predicate (second argument) returns true.
+ * Drop the elements of an array (first argument), starting
+ * from the front, until the predicate (second argument) returns true.
  *
  */
 
 
 function drop(arr, func) {
-	// Drop them elements.
-	return arr;
+	var result = [];
+
+	var index = arr.findIndex(v => func(v));
+
+	if (~index) {
+		result = arr.slice(index);
+	}
+
+	return result;
 }
 
 
---
+++ test/bonfires/intermediate-algorithm-scripting/17-drop-it-spec.js
@@ -0,0 +1,33 @@
+module.exports = function () {
+	'use strict';
+
+	var expect = require("chai")
+		.expect;
+
+	var drop = require("./intermediate-algo-collection")
+		.drop;
+
+	var tests = require("./test-cases/17-test.js");
+
+	describe("Drop It", () => {
+		describe("drop()", () => {
+			it("should return an array", () => {
+				tests.forEach(test => {
+					let result = drop(...test.input);
+
+					expect(result)
+						.to.be.an("array");
+				});
+			});
+
+			it("should drop elements until the predicate returns true", () => {
+				tests.forEach(test => {
+					let result = drop(...test.input);
+
+					expect(result)
+						.to.deep.equal(test.expected);
+				});
+			});
+		});
+	});
+};
