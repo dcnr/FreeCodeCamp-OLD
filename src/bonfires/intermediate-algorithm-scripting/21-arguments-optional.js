a2590b3e66bf19c5fa5fd5435ff6b670ece8950b
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-18 11:50:37 +0800

    Added base files for Bonfire: Arguments Optional.

---
+++ src/bonfires/intermediate-algorithm-scripting/21-arguments-optional.js
@@ -0,0 +1,29 @@
+/* Bonfire: Arguments Optional
+ * http://www.freecodecamp.com/challenges/bonfire-arguments-optional
+ *
+ * Create a function that sums two arguments together. If only one
+ * argument is provided, then return a function that expects one argument
+ * and returns the sum.
+ *
+ * For example, add(2, 3) should return 5, and add(2) should return
+ * a function.
+ *
+ * Calling this returned function with a single argument will then
+ * return the sum:
+ *
+ * var sumTwoAnd = add(2);
+ *
+ * sumTwoAnd(3) returns 5.
+ *
+ * If either argument isn't a valid number, return undefined.
+ *
+ */
+
+
+function add() {
+	return false;
+}
+
+
+/* exports */
+module.exports = add;
