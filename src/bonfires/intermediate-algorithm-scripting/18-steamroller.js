19489eed9cd57369fd40f4416057c090b9d75e93
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-18 09:10:05 +0800

    Temporary working solution.

--- src/bonfires/intermediate-algorithm-scripting/18-steamroller.js
+++ src/bonfires/intermediate-algorithm-scripting/18-steamroller.js
@@ -7,8 +7,46 @@
 
 
 function steamroller(arr) {
-	// I'm a steamroller, baby
-	return arr;
+	'use strict';
+
+	// function process(v) {
+	//
+	// }
+
+	var clone = arr.slice(0);
+	var result = [];
+
+	clone.forEach(v => {
+
+		if (v instanceof Array) {
+			let tmp;
+
+			while (v instanceof Array && v.length) {
+				tmp = v.shift();
+
+				if (tmp instanceof Array) {
+					if (tmp.length === 1 && !(tmp[0] instanceof Array)) {
+						result.push(tmp[0]);
+						break;
+					}
+
+					v = tmp;
+				} else {
+					result.push(tmp);
+				}
+			}
+
+		}
+		else {
+			result.push(v);
+		}
+	});
+
+	// for (let i = 0, len = clone.length; i < len; ++i) {
+	//
+	// }
+
+	return result;
 }
 
 
---
+++ test/bonfires/intermediate-algorithm-scripting/18-steamroller-spec.js
@@ -0,0 +1,35 @@
+module.exports = function () {
+	'use strict';
+
+	var expect = require("chai")
+		.expect;
+
+	var steamroller = require("./intermediate-algo-collection")
+		.steamroller;
+
+	var tests = require("./test-cases/18-test.js");
+
+	describe("Steamroller", () => {
+		describe("steamroller()", () => {
+			it.only("should return an array", () => {
+				tests.forEach(test => {
+					let result = steamroller(test.input);
+
+					expect(result)
+						.to.be.an("array");
+
+						console.log("test.input now:", test.input);
+				});
+			});
+
+			it("should return a flattened array, removing any nesting", () => {
+				tests.forEach(test => {
+					let result = steamroller(test.input);
+
+					expect(result)
+						.to.deep.equal(test.expected);
+				});
+			});
+		});
+	});
+};
