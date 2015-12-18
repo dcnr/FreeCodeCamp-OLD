dcd9c3d5962c0ed9840521f06a0d0cc7b8cbb7b9
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-18 11:26:10 +0800

    Added test cases.

---
+++ src/bonfires/intermediate-algorithm-scripting/20-everything-be-true.js
@@ -0,0 +1,17 @@
+/* Bonfire: Everything Be True
+ * http://www.freecodecamp.com/challenges/bonfire-everything-be-true
+ *
+ * Check if the predicate (second argument) is truthy on all elements
+ * of a collection (first argument).
+ *
+ */
+
+
+function every(collection, pre) {
+	// Is everyone being true?
+	return pre;
+}
+
+
+/* exports */
+module.exports = every;
