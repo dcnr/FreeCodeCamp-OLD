a858c33034031cc4c7357ae547cd21a0a5b6c098
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-19 12:55:58 +0800

    Completed Bonfire: Map the Debris.

--- src/bonfires/upper-intermediate-algorithm-scripting/02-map-the-debris.js
+++ src/bonfires/upper-intermediate-algorithm-scripting/02-map-the-debris.js
@@ -19,9 +19,42 @@
 
 
 function orbitalPeriod(arr) {
-	var GM = 398600.4418;
-	var earthRadius = 6367.4447;
-	return arr;
+	'use strict';
+
+	const GM = 398600.4418;
+	const earthRadius = 6367.4447;
+	let results = [];
+
+
+	function getPeriod(alt) {
+		const a = alt + earthRadius;
+
+		return Math.round(2 * Math.PI * Math.sqrt(Math.pow(a, 3) / GM));
+	}
+
+	function OrbitalData(name, period) {
+		this._name = name;
+		this._orbitalPeriod = period;
+	}
+
+	OrbitalData.prototype.createData = function () {
+		return {
+			name: this._name,
+			orbitalPeriod: this._orbitalPeriod
+		};
+	};
+
+	for (let i = arr.length - 1; i >= 0; --i) {
+		const name = arr[i].name;
+		const avgAlt = arr[i].avgAlt;
+		const op = getPeriod(avgAlt);
+		const data = new OrbitalData(name, op);
+
+		results.push(data.createData());
+	}
+
+	// reversed to match FCC checker
+	return results.reverse();
 }
 
 
---
+++ test/bonfires/upper-intermediate-algorithm-scripting/02-map-the-debris-spec.js
@@ -0,0 +1,49 @@
+module.exports = function () {
+	'use strict';
+
+	const expect = require("chai")
+		.expect;
+
+	const orbitalPeriod = require("./upper-algo-collection")
+		.orbitalPeriod;
+
+	describe("Orbital Period", () => {
+		describe("orbitalPeriod()", () => {
+			it("should return an array", () => {
+				const result = orbitalPeriod([{
+					name: "sputnik",
+					avgAlt: 35873.5553
+					}]);
+
+				expect(result)
+					.to.be.an("array");
+			});
+
+			it("should return an array of objects with their " +
+				"orbital period computed.", () => {
+					const result = orbitalPeriod([{
+						name: "iss",
+						avgAlt: 413.6
+						}, {
+						name: "hubble",
+						avgAlt: 556.7
+						}, {
+						name: "moon",
+						avgAlt: 378632.553
+						}]);
+
+					expect(result)
+						.to.deep.equal([{
+							name: "iss",
+							orbitalPeriod: 5557
+							}, {
+							name: "hubble",
+							orbitalPeriod: 5734
+							}, {
+							name: "moon",
+							orbitalPeriod: 2377399
+							}]);
+				});
+		});
+	});
+};
