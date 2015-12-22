013e6fc872fbde11b024ec23fd3c3130ffda938e
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-22 11:40:52 +0800

    Added base files for Bonfire: Symmetric Difference.

---
+++ src/bonfires/advanced-algorithm-scripting/02-symmetric-difference.js
@@ -0,0 +1,16 @@
+/* Bonfire: Symmetric Difference
+ * Create a function that takes two or more arrays and returns an array of
+ * the symmetric difference of the provided arrays.
+ *
+ * The mathematical term symmetric difference refers to the elements in two
+ * sets that are in either the first or second set, but not in both.
+ *
+ */
+
+
+function sym(args) {
+	return args;
+}
+
+
+module.exports = sym;
--- src/bonfires/advanced-algorithm-scripting/index.js
+++ src/bonfires/advanced-algorithm-scripting/index.js
@@ -1,5 +1,6 @@
 const AdvancedAlgo = {
 	"telephoneCheck": require("./01-validate-us-telephone-numbers"),
+	"sym": require("./02-symmetric-difference"),
 };
 
 module.exports = AdvancedAlgo;
