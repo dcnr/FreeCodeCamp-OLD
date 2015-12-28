b689cf08af3d16ec06acbba2af74dae849bc0cea
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-28 14:25:45 +0800

    Reworked entire testing procedure.
    The original procedure involves setting a variable with
    recognized keywords representing the different suites.
    This limits the testing to suites and does not allow the
    testing and picking of individual files.
    
    The new procedure makes use of an environment variable set by
        export TESTS="keyword:1,2,3;keyword2:all;keyword3:4...6"
    
    This env variable TESTS is a strin,  parsed by the test
    master file to select the tests to be processed.
    
    Suites are set by recognized keywords:
    basic
    intermediate
    upper
    advanced
    all
    
    followed by a ":" and a comma delimited list of file numbers.
    A range may also specified by using an ellipsis.
    
    Example:
        export TESTS="basic:5...10"
    
    Will set the test collection to files 5, 6, 7, 8, 9, 10 from the
    basic suite.
    
    To specify multiple suites, the keywords are separated by
    a semicolon.
    
    Example:
        export TESTS="upper:1,2,3;intermediate:all"
    
    This will set the test collection to files 1, 2, 3 for the upper
    suite and all of the intermediate suite.
    
    "all" is a special keyword that can be specified alone or with
    another keyword.
    
    Example:
        export TESTS="all" // will test all suites
    	 export TESTS="basic:all" // will test all basic suite tests

--- test/index.js
+++ test/index.js
@@ -1,109 +1,33 @@
+const setPath = require("./paths");
+
 (function () {
-  'use strict';
+   'use strict';
 
-  // Accepted values:
-  // basic = basic scripting algorithm tests
-  // intermediate = intermediate scripting algorithm tests
-  // upper = upper intermediate scripting algorithm tests
-  // advanced = advanced algorithm scripting
-  // all = both basic and intermediate
-
-  var test_suite = "advanced";
+   const conf = {};
+   let collection = [];
+   let env = process.env.TESTS || "all";
 
 
+   if (env !== "all") {
+      env.split(";").forEach(suite => {
+         suite = suite.split(":");
+         const type = suite[0];
+         const filelist = suite[1];
 
+         conf[type] = setPath(type, filelist);
+      });
+   }
+   else {
+      conf.all = setPath("all");
+   }
 
-  /********************** DON'T MODIFY BELOW THIS ***************************/
+   Object.keys(conf).forEach(key => {
+      collection = collection.concat(conf[key]);
+   });
 
-  const basic = "./bonfires/basic-algorithm-scripting/";
-  const intermediate = "./bonfires/intermediate-algorithm-scripting/";
-  const upper = "./bonfires/upper-intermediate-algorithm-scripting/";
-  const advanced = "./bonfires/advanced-algorithm-scripting/";
-
-  const basic_suite = [
-    require(basic + "02-reverse-a-string-spec"),
-    require(basic + "03-factorialize-a-number-spec"),
-    require(basic + "04-check-for-palindromes-spec"),
-    require(basic + "05-find-the-longest-word-in-a-string-spec"),
-    require(basic + "06-title-case-a-sentence-spec"),
-    require(basic + "07-return-largest-numbers-in-arrays-spec"),
-    require(basic + "08-confirm-the-ending-spec"),
-    require(basic + "09-repeat-a-string-repeat-a-string-spec"),
-    require(basic + "10-truncate-a-string-spec"),
-    require(basic + "11-chunky-monkey-spec"),
-    require(basic + "12-slasher-flick-spec"),
-    require(basic + "13-mutations-spec"),
-    require(basic + "14-falsy-bouncer-spec"),
-    require(basic + "15-seek-and-destroy-spec"),
-    require(basic + "16-where-do-i-belong-spec")
-  ];
-
-  const intermediate_suite = [
-    require(intermediate + "01-sum-all-numbers-in-a-range-spec"),
-    require(intermediate + "02-diff-two-arrays-spec"),
-    require(intermediate + "03-roman-numeral-converter-spec"),
-    require(intermediate + "04-where-art-thou-spec"),
-    require(intermediate + "05-search-and-replace-spec"),
-    require(intermediate + "06-pig-latin-spec"),
-    require(intermediate + "07-dna-pairing-spec"),
-    require(intermediate + "08-missing-letters-spec"),
-    require(intermediate + "09-boo-who-spec"),
-    require(intermediate + "10-sorted-union-spec"),
-    require(intermediate + "11-convert-html-entities-spec"),
-    require(intermediate + "12-spinal-tap-case-spec"),
-    require(intermediate + "13-sum-all-odd-fibonacci-numbers-spec"),
-    require(intermediate + "14-sum-all-primes-spec"),
-    require(intermediate + "15-smallest-common-multiple-spec"),
-    require(intermediate + "16-finders-keepers-spec"),
-    require(intermediate + "17-drop-it-spec"),
-    require(intermediate + "18-steamroller-spec"),
-    require(intermediate + "19-binary-agents-spec"),
-    require(intermediate + "20-everything-be-true-spec"),
-    require(intermediate + "21-arguments-optional-spec")
-  ];
-
-  const upper_suite = [
-    require(upper + "01-make-a-person-spec"),
-    require(upper + "02-map-the-debris-spec"),
-    require(upper + "03-pairwise-spec")
-  ];
-
-  const advanced_suite = [
-    require(advanced + "01-validate-us-telephone-numbers-spec"),
-    require(advanced + "02-symmetric-difference-spec"),
-    require(advanced + "03-exact-change-spec"),
-    require(advanced + "04-inventory-update-spec"),
-  ];
-
-
-  let tests;
-  switch (test_suite) {
-    case 'basic':
-      tests = basic_suite;
-      break;
-
-    case 'intermediate':
-      tests = intermediate_suite;
-      break;
-
-    case 'upper':
-      tests = upper_suite;
-      break;
-
-    case 'advanced':
-      tests = advanced_suite;
-      break;
-
-    case 'all':
-      tests = basic_suite.concat(intermediate_suite, upper_suite);
-      break;
-
-    default:
-      tests = basic_suite;
-  }
-
-
-  tests.forEach(
-    test => test()
-  );
-})();
+   collection.forEach(test => {
+      if (test instanceof Function) {
+         test();
+      }
+   });
+}());
