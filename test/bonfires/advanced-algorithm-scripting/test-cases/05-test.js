d8443ea078a17fea0fd26d632c2311c3714ebf5d
Norman Dela Cruz <normandelacruz@celestialhues.com>
2015-12-31 19:05:58 +0800

    Added Bonfire: No Repeats Please
    Given a string, this program returns the total number
    of permutations that doesn't have consecutive characters.
    
    It makes use of Heap's Algorithm to create all the
    permutations and then each is checked for consecutive
    characters.
    
    NOTE: A brute force solution, should be revisited

---
+++ test/bonfires/advanced-algorithm-scripting/test-cases/05-test.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    "input": "aab",
+    "expected": 2
+  },
+  {
+    "input": "aaa",
+    "expected": 0
+  },
+  {
+    "input": "aabb",
+    "expected": 8
+  },
+  {
+    "input": "abcdefa",
+    "expected": 3600
+  },
+  {
+    "input": "abfdefa",
+    "expected": 2640
+  },
+  {
+    "input": "zzzzzzzz",
+    "expected": 0
+  },
+];
