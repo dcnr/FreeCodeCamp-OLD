88852a645af39d152d73299bc8dbc7273c1744ba
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-01-11 06:04:11 +0800

    Refactored Diff Two Arrays
    Considerably reduced code by just using filter instead of
    manually looping through each element and pushing off
    duplicates.

--- src/bonfires/intermediate-algorithm-scripting/02-diff-two-arrays.js
+++ src/bonfires/intermediate-algorithm-scripting/02-diff-two-arrays.js
@@ -10,24 +10,10 @@
 function diff(arr1, arr2) {
   'use strict';
 
-  const newArray = Array.prototype.concat(...arguments)
-    .sort();
-
-  let result = [];
-
-
-  for (let i = 0, len = newArray.length; i < len; i++) {
-    const current_item = newArray[i];
-    const next_item = newArray[i + 1];
-
-    if (current_item !== next_item) {
-      result.push(current_item);
-      continue;
-    }
-
-    i++;
-  }
-
+  const result = [].concat(
+    arr1.filter(v => !~arr2.indexOf(v)),
+    arr2.filter(v => !~arr1.indexOf(v))
+  );
 
   return result;
 }
