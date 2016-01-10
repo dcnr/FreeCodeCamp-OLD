a6c540d8040121b1587e87d0e9ed884c26a891f2
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-01-11 07:23:39 +0800

    Refactored Roman Numeral Converter
    Added test cases and skipped the string concatenation
    by using unshift.

--- src/bonfires/intermediate-algorithm-scripting/03-roman-numeral-converter.js
+++ src/bonfires/intermediate-algorithm-scripting/03-roman-numeral-converter.js
@@ -11,15 +11,6 @@
 function convert(num) {
   'use strict';
 
-  if (num === 0) {
-    return 'nulla';
-  }
-
-  if (num >= 4000) {
-    return 'Sorry, currently the converter cannot ' +
-      'deal with numbers greater than 3999.';
-  }
-
   const roman_numerals = [
     ['', 'M', 'MM', 'MMM'],
     ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
@@ -27,21 +18,121 @@ function convert(num) {
     ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
   ];
 
-  let digits = num.toString();
+
+  let digits = num
+    .toString()
+    .split('');
+
   while (digits.length < 4) {
-    digits = '0' + digits;
+    digits.unshift('0');
   }
 
-  const converted_numeral = digits
-    .split('')
-    .map(digit => parseInt(digit))
-    .map((digit, index) => roman_numerals[index][digit])
+  const converted = digits
+    .map((d, i) => roman_numerals[i][parseInt(d, 10)])
     .join('');
 
 
-  return converted_numeral;
+  return converted;
 }
 
 
 /* exports */
 module.exports = convert;
+
+
+/* module ran directly */
+if (require.main === module) {
+  console.log(
+    `
+Roman Numeral Converter\n
+  Convert a given number into a Roman Numeral\n`
+  );
+
+  console.log(
+    '\t5\t=>\t', convert(5)
+  );
+
+  console.log(
+    '\t9\t=>\t', convert(9)
+  );
+
+  console.log(
+    '\t12\t=>\t', convert(12)
+  );
+
+  console.log(
+    '\t16\t=>\t', convert(16)
+  );
+
+  console.log(
+    '\t29\t=>\t', convert(29)
+  );
+
+  console.log(
+    '\t44\t=>\t', convert(44)
+  );
+
+  console.log(
+    '\t45\t=>\t', convert(45)
+  );
+
+  console.log(
+    '\t68\t=>\t', convert(68)
+  );
+
+  console.log(
+    '\t83\t=>\t', convert(83)
+  );
+
+  console.log(
+    '\t97\t=>\t', convert(97)
+  );
+
+  console.log(
+    '\t99\t=>\t', convert(99)
+  );
+
+  console.log(
+    '\t500\t=>\t', convert(500)
+  );
+
+  console.log(
+    '\t501\t=>\t', convert(501)
+  );
+
+  console.log(
+    '\t649\t=>\t', convert(649)
+  );
+
+  console.log(
+    '\t798\t=>\t', convert(798)
+  );
+
+  console.log(
+    '\t891\t=>\t', convert(891)
+  );
+
+  console.log(
+    '\t1000\t=>\t', convert(1000)
+  );
+
+  console.log(
+    '\t1004\t=>\t', convert(1004)
+  );
+
+  console.log(
+    '\t1006\t=>\t', convert(1006)
+  );
+
+  console.log(
+    '\t1023\t=>\t', convert(1023)
+  );
+
+  console.log(
+    '\t2014\t=>\t', convert(2014)
+  );
+
+  console.log(
+    '\t3999\t=>\t', convert(3999)
+  );
+}
