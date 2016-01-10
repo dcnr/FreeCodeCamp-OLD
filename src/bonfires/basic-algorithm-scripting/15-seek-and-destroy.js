b45f16c2614de7b480c0c1fa7e8be59fecd3fc79
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-01-10 11:31:52 +0800

    Refactored Seek and Destroy
    Shortened code by removing the redundant IF statements
    in the filter function.
    
    Also changed the arguments object copying method.

--- src/bonfires/basic-algorithm-scripting/15-seek-and-destroy.js
+++ src/bonfires/basic-algorithm-scripting/15-seek-and-destroy.js
@@ -13,19 +13,14 @@
 function destroyer(arr) {
   'use strict';
 
-  let args = [];
-  for (let i = 0; i < arguments.length; ++i) {
-    args.push(arguments[i]);
+  const len = arguments.length;
+  const args = Array.call(null, new Array(len));
+  for (let i = 0; i < len; ++i) {
+    args[i] = arguments[i];
   }
 
 
-  arr = arr.filter(val => {
-    if (~args.indexOf(val)) {
-      return false;
-    }
-
-    return true;
-  });
+  arr = arr.filter(val => !~args.indexOf(val));
 
   return arr;
 }
