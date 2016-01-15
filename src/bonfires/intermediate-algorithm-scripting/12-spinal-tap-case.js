19f7c15bd0a2d3cf4619149bb9477370162d0bde
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-01-15 12:57:29 +0800

    Refactored Spinal Tap Case
    Made a better regular expression pattern and used it with
    replace.

--- src/bonfires/intermediate-algorithm-scripting/12-spinal-tap-case.js
+++ src/bonfires/intermediate-algorithm-scripting/12-spinal-tap-case.js
@@ -10,21 +10,9 @@
 function spinalCase(str) {
   'use strict';
 
-  let result = str;
-  const re_spaces = new RegExp(/[\s_]/g);
-  const re_nospaces = new RegExp(/(.)([A-Z])/g);
-
-  if (~result.indexOf(" ") || ~result.indexOf("_")) {
-    result = result.replace(re_spaces, "-");
-  }
-  else {
-    result = result.replace(re_nospaces, "$1-$2");
-  }
-
-  result = result.toLowerCase();
-
-
-  return result;
+  return str
+    .replace(/[_ ]|([a-z])([A-Z])/g, '$1-$2')
+    .toLowerCase();
 }
 
 
