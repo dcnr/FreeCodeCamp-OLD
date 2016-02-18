c14fde37c216b7e78b490cab1dc16a1133f803c2
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-02-18 11:16:13 +0800

    Removed methods in favor of for loop
    Using string and array methods is costly and for loops
    are about twice faster.

--- src/challenges/basic-algorithm-scripting/02-reverse-a-string.js
+++ src/challenges/basic-algorithm-scripting/02-reverse-a-string.js
@@ -14,10 +14,13 @@
 function reverseString(str) {
   'use strict';
 
-  return str
-    .split('')
-    .reverse()
-    .join('');
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; --i) {
+    reversed += str[i];
+  }
+
+  return reversed;
 }
 
 
